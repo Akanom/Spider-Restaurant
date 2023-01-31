0cdd6bcc9e0bbc06341d8a2ccbbad9e78ced2140
Mazstar04 <mazeedahhamzat@gmail.com>
2023-01-31 22:34:57 +0100

    fixed product detail bug and implemented search functionality

--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,7 +1,12 @@
-import  DATA  from "../DATA";
+import DATA from "../DATA";
 import { NavLink } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export default function Products() {
+
+  const [displayProducts, setDisplayProducts] = useState(DATA)
+  const [searchValue, setSearchValue] = useState("");
+
   const cardItem = (item) => {
     return (
       <div className="card  my-5 py-4" key={item.id} style={{ width: "20rem" }}>
@@ -20,6 +25,11 @@ export default function Products() {
     );
   };
 
+  useEffect(() => {
+    console.log("sear", searchValue);
+    let data = DATA.filter(f => f.title.toLowerCase().includes(searchValue.toLowerCase()));
+    setDisplayProducts(data);
+  }, [searchValue]);
   return (
     <div>
       <div className="container py-5">
@@ -30,8 +40,30 @@ export default function Products() {
           </div>
         </div>
       </div>
+      <div className="row  ">
+        <form className="d-flex w-75 mx-auto">
+          <input
+            className="form-control me-2"
+            type="search"
+            placeholder="Search"
+            aria-label="Search"
+            value={searchValue}
+            onChange={(e) => { setSearchValue(e.target.value) }}
+          />
+          <div className="btn btn-outline-success" >
+            Search
+          </div>
+        </form>
+      </div>
       <div className="container">
-        <div className="row justify-content-around">{DATA.map(cardItem)}</div>
+        {
+          displayProducts.length > 0 &&
+          <div className="row justify-content-around">{displayProducts.map(cardItem)}</div>
+        }
+        {
+          displayProducts.length == 0  &&
+          <div className="row justify-content-around text-muted display-5" style={{margin: "100px 0"}}>No Results for {searchValue}</div>
+        }
       </div>
     </div>
   );
